0eb09c6722c0ad93868905517cb9aaf90c41c39e
liucaidong <791136794@qq.com>
2018-05-03 02:43:45 +0800

    first commit

---
+++ src/main/java/cn/psa/mres/MresApplication.java
@@ -0,0 +1,12 @@
+package cn.psa.mres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MresApplication.class, args);
+    }
+}
